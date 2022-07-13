9652ef1e1167c6a0cb3a64a7e501061129fe4ac3
seushin <seushin@student.42seoul.kr>
2022-07-13 23:04:33 +0900

    add test case

--- test/vector/iterator.cpp
+++ test/vector/iterator.cpp
@@ -10,6 +10,44 @@ Test(vector_iter, iterator_basic)
 
 	begin = v.begin();
 	end = v.end();
+
+	cr_expect_eq(*begin, v[0]);
+
+	*begin = 0;
+	cr_expect_eq(v[0], 0);
+
+	cr_expect_eq(begin + v.size(), end);
+}
+
+Test(vector_iter, iterator_iteration)
+{
+	const int n = 10;
+	ft::vector<int> v;
+	ft::vector<int>::iterator begin, end;
+
+	for (int i = 0; i < n; ++i)
+		v.push_back(i);
+
+	begin = v.begin();
+	end = v.end();
+
+	int i = 0;
+	for (ft::vector<int>::iterator it = begin; it != end; ++it)
+	{
+		cr_expect_eq(*it, i);
+		i++;
+	}
+	cr_expect_eq(i, v.size());
+}
+
+Test(vector_iter, const_iterator_basic)
+{
+	const ft::vector<int> v(10, 42);
+	ft::vector<int>::const_iterator begin, end;
+
+	begin = v.begin();
+	end = v.end();
+
 	cr_expect_eq(*begin, v[0]);
 	cr_expect_eq(begin + v.size(), end);
 }
